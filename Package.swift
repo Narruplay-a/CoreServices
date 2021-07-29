abce00df8ea8c3191ebf6f0b86e7294531ee9d81
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2021-07-29 12:58:43 +0300

    fix deployment target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CoreServicePackage",
+    platforms: [
+       .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -26,5 +29,5 @@ let package = Package(
         .testTarget(
             name: "CoreServicePackageTests",
             dependencies: ["CoreServicePackage"]),
-    ]
+    ],
 )
