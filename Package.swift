468cba5e6cbcadd9b7687ffe5783386a4314d7b3
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2021-07-29 13:13:27 +0300

    fix dependencies

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CoreServicePackage",
-            dependencies: []),
+            dependencies: ["AnyCodable"]),
         .testTarget(
             name: "CoreServicePackageTests",
             dependencies: ["CoreServicePackage"]),
